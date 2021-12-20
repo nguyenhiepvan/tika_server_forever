17089cfe00b6dea9415af3214156ae4e00fb8bec
Nguyễn Văn Hiệp <nguyenhiepvan.bka@gmail.com>
2021-12-20 21:31:12 +0700

    update download and check server health

--- check_tika_health.sh
+++ check_tika_health.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-TIKA_URL="http://127.0.0.1:9998/tika"
+TIKA_URL="http://127.0.0.1:9998"
 
 sleep 5
 
-RESPONSE=$(curl -m 5 --write-out "%{http_code}\n" --silent --output /dev/null -T test.pdf ${TIKA_URL})
+RESPONSE=$(curl -m 5 --write-out "%{http_code}\n" --silent --output /dev/null ${TIKA_URL})
 
-if [ $RESPONSE != '200' ]
+if [ "$RESPONSE" != '200' ]
 then
     echo Restarting tika
     pm2 delete "tika_server"
+    # shellcheck disable=SC2046
     cd $(pwd) && pm2 start pm2.yml
 else
     echo tika_server is running normally
--- download_tika.sh
+++ download_tika.sh
@@ -2,5 +2,5 @@
 
 VERSION=${1:-'1.24.1'}
 
-curl -o "tika-server.jar" "http://mirror.downloadvn.com/apache/tika/tika-server-${VERSION}.jar" \
-|| wget -o "tika-server.jar" "http://mirror.downloadvn.com/apache/tika/tika-server-${VERSION}.jar" \
+curl -o "./tika-server.jar" "https://archive.apache.org/dist/tika/tika-server-${VERSION}.jar" \
+|| wget -o "./tika-server.jar" "https://archive.apache.org/dist/tika/tika-server-${VERSION}.jar" \
