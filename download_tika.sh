0b49cacb638e6d233cf8f265d5bcb37e28f71738
Nguyễn Văn Hiệp <nguyenhiepvan.bka@gmail.com>
2022-07-22 15:58:58 +0700

    using tika 2.4.1

--- download_tika.sh
+++ download_tika.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-VERSION=${1:-'1.24.1'}
-
-curl -o "./tika-server.jar" "https://archive.apache.org/dist/tika/tika-server-${VERSION}.jar" \
-|| wget -o "./tika-server.jar" "https://archive.apache.org/dist/tika/tika-server-${VERSION}.jar" \
+curl -o "./tika-server.jar" "https://archive.apache.org/dist/tika/2.4.1/tika-server-standard-2.4.1.jar" \
+|| wget -o "./tika-server.jar" "https://archive.apache.org/dist/tika/2.4.1/tika-server-standard-2.4.1.jar" \
